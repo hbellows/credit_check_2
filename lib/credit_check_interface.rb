ef8a2876a0e5569fcdf4fcb7d344a2a147331132
Harper Bellows <harper.bellows@gmail.com>
2018-06-27 06:47:47 -0600

    add command line interface file for credit check

---
+++ lib/credit_check_interface.rb
@@ -0,0 +1,21 @@
+require './lib/credit_check'
+
+puts "*" * 50
+puts "\n"
+puts "Credit Card Validation"
+puts "\n"
+puts "*" * 50
+puts "\n"
+puts "Please enter your credit card number here:"
+puts "\n"
+print "> "
+
+user_input = gets.chomp
+card_number = user_input.to_i
+
+credit_check = CreditCheck.new
+
+puts "\n"
+puts credit_check.validation_output(card_number)
+puts "\n"
+puts "*" * 50
