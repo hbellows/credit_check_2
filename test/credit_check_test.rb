d07e5734923f062b230e4dc57927b8268b0b6d0e
Harper Bellows <harper.bellows@gmail.com>
2018-06-27 06:47:22 -0600

    finalize testing functionality validation output functionality

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -5,9 +5,8 @@ require './lib/credit_check'
 class CreditCheckTest < Minitest::Test
 
   def test_it_converts_card_number_to_an_array_of_integers
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     result = [0, 4, 2, 5, 9, 7, 3, 2, 9, 8, 0, 8, 1, 4, 5, 5]
 
@@ -15,9 +14,8 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_double_every_other_number
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     converted = [0, 4, 2, 5, 9, 7, 3, 2, 9, 8, 0, 8, 1, 4, 5, 5]
 
@@ -27,9 +25,8 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_sum_double_digit_numbers
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     doubled = [0, 8, 2, 10, 9, 14, 3, 4, 9, 16, 0, 16, 1, 8, 5, 10]
 
@@ -39,9 +36,8 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_sum_all_digits
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     summed_digits = [0, 8, 2, 1, 9, 5, 3, 4, 9, 7, 0, 7, 1, 8, 5, 1]
 
@@ -49,9 +45,8 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_validate_a_card_number
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     totaled_digits = 70
 
@@ -59,19 +54,29 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_determine_validity_of_a_card_number
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     assert_equal true, credit_check.valid_number?(card_number)
+
+    card_number = 6011797668868728
+    credit_check = CreditCheck.new
+
+    assert_equal false, credit_check.valid_number?(card_number)
   end
 
   def test_it_can_send_the_correct_validation_message
-    # skip
-    card_number = '5541808923795240'
-    credit_check = CreditCheck.new #(card_number)
+    card_number = 5541808923795240
+    credit_check = CreditCheck.new
 
     result = "The number 5541808923795240 is valid."
+
+    assert_equal result, credit_check.validation_output(card_number)
+
+    card_number = 6011797668868728
+    credit_check = CreditCheck.new
+
+    result = "The number 6011797668868728 is invalid."
 
     assert_equal result, credit_check.validation_output(card_number)
   end
